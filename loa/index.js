83a2c4c1568162fe3f4a9f06e93c2491dd23a3e8
ejaz-aiman <amanejaz00@gmail>
2020-02-22 00:38:55 +0530

    Minor changes

--- loa/index.js
+++ loa/index.js
@@ -9,7 +9,7 @@ const qs = require("qs");
 const porting = express();
 porting.use(cors());
 
-porting.use(bodyParser.json({ limit: "10mb" }));
+porting.use(bodyParser.json({ limit: "8mb" }));
 porting.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 porting.use("/", (req, res, next) => {
